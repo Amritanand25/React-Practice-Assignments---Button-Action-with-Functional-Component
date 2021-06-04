3d8cfb245946e99fd502fdd706e49be2478a074c
Amritanand25 <amritanand.jnv25@gmail.com>
2021-06-04 16:17:15 +0530

    next
    next

--- src/components/Render.js
+++ src/components/Render.js
@@ -5,11 +5,12 @@ function Render()
     const [data, setData] = useState('');
  function btnHandler()
  {
-  setData("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+  setData("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.");
  }
  return (
  <div>
-     <p id="para">{data}</p>
+    
+    <p id="para">{data}</p>
      <button id="click" onClick={btnHandler}>Click</button>
  </div>
  );
