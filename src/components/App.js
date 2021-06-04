d61e3ce26ae31b7d182c6fc58c07b41cdb439d34
Amritanand25 <amritanand.jnv25@gmail.com>
2021-06-04 16:09:51 +0530

    rendered
    paragraph is rendered

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
+import Render from "./Render"
 
 function App() {
   return (
     <div id="main">
-      // Do not alter the main div
+      <Render />
     </div>
   );
 }
---
+++ src/components/Render.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react"
+
+function Render()
+{
+    const [data, setData] = useState('');
+ function btnHandler()
+ {
+  setData("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+ }
+ return (
+ <div>
+     <p id="para">{data}</p>
+     <button id="click" onClick={btnHandler}>Click</button>
+ </div>
+ );
+}
+export default Render;
